240a7edf82e665c0382e4f9c61769f64f7e1dec8
qichanna <qichanna@126.com>
2017-04-23 14:10:55 +0800

    siderbar js add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-/**
- * Created by liqi on 2017/4/22.
- */
+;$(function () {
+    'user strict';
+
+    var sidebar = $('#sidebar'),
+        mask = $('.mask'),
+        sidebar_trigger = $('#sidebar_trigger');
+    
+    function showSideBar() {
+        mask.fadeIn();
+        // sidebar.animate({'right':0});
+        sidebar.css('right',0);
+    }
+
+    function hideSideBar() {
+        mask.fadeOut();
+        // sidebar.animate({'right':0});
+        sidebar.css('right',-sidebar.width());
+    }
+    
+    sidebar_trigger.on('click',showSideBar);
+    mask.on('click',hideSideBar);
+})
