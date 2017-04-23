9a1e14fe59d475c084ae298c35e49022ca65245f
qichanna <qichanna@126.com>
2017-04-23 14:54:28 +0800

    back top add

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
     var sidebar = $('#sidebar'),
         mask = $('.mask'),
+        backButton = $('.back-to-top'),
         sidebar_trigger = $('#sidebar_trigger');
     
     function showSideBar() {
@@ -19,4 +20,18 @@
     
     sidebar_trigger.on('click',showSideBar);
     mask.on('click',hideSideBar);
+    backButton.on('click',function () {
+        $('html,body').animate({
+            scrollTop:0
+        },800)
+    });
+    $(window).on("scroll",function () {
+        if($(window).scrollTop() > $(window).height()){
+            backButton.fadeIn();
+        }else {
+            backButton.fadeOut();
+        }
+    })
+
+    $(window).trigger('scroll');
 })
